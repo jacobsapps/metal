3a7683124d0271ca5753d8d8b81e156b1490a7d4
Jacob Bartlett <>
2023-10-26 21:05:34 +0100

    Initial commit

---
+++ LearnMetal/LearnMetal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnMetal
+//
+//  Created by Jacob Bartlett on 26/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnMetal/LearnMetal/LearnMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMetalApp.swift
+//  LearnMetal
+//
+//  Created by Jacob Bartlett on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
